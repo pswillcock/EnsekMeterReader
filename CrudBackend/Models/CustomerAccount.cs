58e611e2d23d485db32ff4079ed9d5b3bc848783
paul_willcock <pswillcock@googlemail.com>
2020-02-01 17:58:56 +0000

    Merged projects and added dummy placeholder for CSV file parsing backend

---
+++ CrudBackend/Models/CustomerAccount.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ENSEK_Meter_Reader_Server.Models {
+
+    [Table("Accounts")]
+    public class CustomerAccount {
+        [Key]
+        public int AccountId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [ForeignKey("ReadingId")]
+        public List<MeterReading> MeterReadings { get; set; }
+    }
+}
